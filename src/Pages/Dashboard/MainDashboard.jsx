cff960e78f0c1a4bf9ae998931f99041f29e5ebe
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2024-07-17 01:30:04 +0600

    first commit

---
+++ src/Pages/Dashboard/MainDashboard.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const MainDashboard = () => {
+    return (
+        <div>
+           
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default MainDashboard;
